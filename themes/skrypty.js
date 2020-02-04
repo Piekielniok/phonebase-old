363133fe886efa49e1736a9dea2a6d0423738052
Piekielniok <piekielniok@wp.pl>
2020-02-04 22:09:42 +0100

    Usprawnienia w kodzie, początkowe chowanie nagłówka i niepoukładane ikony społeczności na dole strony.

--- themes/skrypty.js
+++ themes/skrypty.js
@@ -4,8 +4,18 @@ window.onscroll = function () {
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").style.height = "4.5vw";
-    } else {
-        document.getElementById("header").style.height = "9.5vw";
+        document.querySelector(".header").style.cssText = "height: 5vw";
+        document.querySelector(".header > div").style.cssText = "display: block; height: 3.7vw; width: 4.1vw; margin-top: 0.5vw; margin-left: 0.5vw; padding-top: 0.4vw; text-align: left;";
+        document.querySelector(".header-logo").innerHTML = "<img style='margin-right: -0.6vw;' src='images/logo-phone.png'><span>P</span>";
+        document.querySelector(".header-logo ~ div").innerHTML = "<img src='images/header-phones.png'><span style='display: inline-block; margin-top: -3vw;'>Telefony</span>";
+        document.querySelector(".header-logo ~ div").style.cssText = "display: inline-block; margin-top: -8vw;";
+    }
+    else {
+        document.querySelector(".header").style.cssText = "height: 9.5vw";
+        document.querySelector(".header-logo").innerHTML = "<img style='margin-right: -0.6vw;' src='images/logo-phone.png'><span>Phone<b>base</b></span>";
+        document.querySelector(".header > div").style.cssText = "margin: 2vw 1vw 2vw 2vw; display: inline-block; height: 4.5vw; width: 20vw; padding-top: 1vw; text-align: center;";
+        
     }
 }
+
+//Sam sobie będziesz kurwa ten chowany pasek sobie ogarniał, bo ja już straciłem do niego cierpliwość...
